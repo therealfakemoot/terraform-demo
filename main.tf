0699a25951b56736c3eb4b5273eddfea9a3c27de
Nick Dumas <dumasn@homes.com>
2019-08-05 12:08:23 -0400

    First draft at implementing a puppet cluster.

--- main.tf
+++ main.tf
@@ -2,62 +2,22 @@ variable "do_token" {}
 variable "ssh_root_pubkey_path" {}
 variable "ssh_root_privkey_path" {}
 variable "demo_domain" {}
+variable "deployment_environment" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+resource "digitalocean_project" "terraform-demo" {
+  name        = "terraform-demo"
+  description = "A workspace for demonstrating terraform projects."
+  purpose     = "Web Application"
+  resources   = ["${digitalocean_droplet.puppet_master.urn}", "${digitalocean_droplet.web.urn}"]
+}
+
 resource "digitalocean_ssh_key" "terraform_key" {
   name       = "terraform_default"
   public_key = "${file(var.ssh_root_pubkey_path)}"
-}
-
-resource "digitalocean_droplet" "web" {
-  image  = "ubuntu-16-04-x64"
-  name   = "terraform-1"
-  region = "nyc3"
-  size   = "s-1vcpu-1gb"
-  # monitoring = true
-  ssh_keys = [digitalocean_ssh_key.terraform_key.id]
-
-  provisioner "remote-exec" {
-    inline = [
-      "apt-get update",
-      "apt-get -y install nginx",
-      "mkdir -p /var/www/demo.noodlemilkhotel.com/",
-    ]
-
-    connection {
-      type        = "ssh"
-      private_key = "${file(var.ssh_root_privkey_path)}"
-      host        = "${digitalocean_droplet.web.ipv4_address}"
-    }
-
-  }
-
-  provisioner "file" {
-    source      = "www/index.html"
-    destination = "/var/www/demo.noodlemilkhotel.com/index.html"
-
-    connection {
-      type        = "ssh"
-      private_key = "${file(var.ssh_root_privkey_path)}"
-      host        = "${digitalocean_droplet.web.ipv4_address}"
-    }
-  }
-
-  provisioner "file" {
-    source      = "www/demo.noodlemilkhotel.com"
-    destination = "/etc/nginx/sites-enabled/demo.noodlemilkhotel.com"
-
-    connection {
-      type        = "ssh"
-      private_key = "${file(var.ssh_root_privkey_path)}"
-      host        = "${digitalocean_droplet.web.ipv4_address}"
-    }
-  }
-
-
 }
 
 resource "digitalocean_record" "demo" {
@@ -66,3 +26,36 @@ resource "digitalocean_record" "demo" {
   name   = "demo"
   value  = digitalocean_droplet.web.ipv4_address
 }
+
+resource "digitalocean_record" "puppet" {
+  domain = "${var.demo_domain}"
+  type   = "A"
+  name   = "puppet"
+  value  = digitalocean_droplet.puppet_master.ipv4_address
+}
+
+resource "digitalocean_droplet" "puppet_master" {
+  image  = "ubuntu-16-04-x64"
+  name   = "puppet-master"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  # monitoring = true
+  ssh_keys = [digitalocean_ssh_key.terraform_key.id]
+
+}
+
+resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-16-04-x64"
+  name   = "web"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  # monitoring = true
+  ssh_keys = [digitalocean_ssh_key.terraform_key.id]
+
+  provisioner "puppet" {
+    server      = "puppet.${var.demo_domain}"
+    open_source = false
+    environment = "${var.deployment_environment}"
+  }
+}
+
